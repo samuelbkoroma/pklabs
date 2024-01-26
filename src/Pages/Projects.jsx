7d188c512bbf4f0a24bf1a5b761b246988a5f236
Power <samuelbkoroma7@gmail.com>
2024-01-26 17:34:30 +0000

    latest

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -175,9 +175,18 @@ const Div = styled.div`
     padding: 10px;
   }
 
+  .project-text h1 {
+    color: black;
+    text-align: left;
+    font-family: "Conthrax", sans-serif;
+    font-size: 25x;
+    font-style: normal;
+    font-weight: 500;
+    text-transform: capitalize;
+  }
   .project-text p {
     color: black;
-    text-align: center;
+    text-align: left;
     font-family: poppins;
     font-size: 19px;
     font-style: normal;
